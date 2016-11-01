de6bd67b1207b000ccf1cb7d49326816121683cc
Michal Vacek <vahyno@gmail.com>
2016-11-01 04:50:00 +0000

    Done.

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,3 +1,4 @@
+puts ""
 puts "Welcome to Tic Tac Toe!"
-# You can see what this file does by running:
+puts ""# You can see what this file does by running:
 # ruby lib/welcome.rb from your terminal.
