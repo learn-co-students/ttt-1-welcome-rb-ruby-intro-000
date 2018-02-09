b2497a93f4505a463334e1c6e1abe68cc326e480
Dennis Medlock <denmedlock@gmail.com>
2018-02-09 14:14:30 +0000

    Done.

--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -1,7 +1,10 @@
 describe "lib/welcome.rb" do
   it 'prints "Welcome to Tic Tac Toe!"' do
-    expect($stdout).to receive(:puts).with("Welcome to Tic Tac Toe!"), "Make sure lib/welcome.rb has code that can output 'Welcome to Tic Tac Toe!' exactly."
-    
+    expect($stdout).to receive(:puts).with("Welcome to Tic Tac Toe!"), "Make
+    me.rb has code that can output 'Welcome to Tic Tac Toe!' exactly."
+
     load './lib/welcome.rb'
-  end
+  endruby lib/welcome.rbruby lib/welcome.rb
 end
+lib/welcome.#!/usr/bin/env ruby -wKU
+puts "Welcome to Tic Tac Toe!"
