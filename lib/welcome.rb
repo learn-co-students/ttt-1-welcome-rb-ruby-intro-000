a59ee98316f9ef7646ac3c7e1e351465f673c97a
darling-proxy-3253 <eyitayoojo28@yahoo.com>
2021-03-17 02:06:05 +0000

    Done.

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,3 +1,5 @@
-# Edit this file to output "Welcome to Tic Tac Toe!"
-# You can see what this file does by running:
+puts "Welcome to Tic Tac Toe"# Edit this file to output "Welcome to Tic Tac Toe!"
+lib/welcom.rb# You can see what this file does by running:
 # ruby lib/welcome.rb from your terminal.
+lib/welcome.#!/usr/bin/env ruby -wKU
+puts "welcome to Tic Tac Toe"
